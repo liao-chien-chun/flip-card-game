2ba6b9ba1b845b63d0f058cadd84869f60266bd8
Eric LIAO <d1044171009@gmail.com>
2022-07-04 23:00:17 +0800

    更改getCardElement的運算邏輯

--- main.js
+++ main.js
@@ -8,19 +8,23 @@ const Symbols = [
 
 const view = {
   //負責生成卡片內容，包括花色和數字
-  getCardElement () {
+  getCardElement (index) {
+    const number = (index % 13) + 1 //ex 1 % 13 = 1在加1就變成2 卡片數字
+
+    //0~12 / 13 捨去小數點 都會是0所以在Symbols 陣列裡都會是索引值0的黑桃
+    const symbol = Symbols[Math.floor(index / 13)]  //取得卡片花色
     return `
       <div class="card">
-        <p>4</p>
-        <img src="https://assets-lighthouse.alphacamp.co/uploads/image/file/17991/diamonds.png" alt="">
-        <p>4</p>
+        <p>${number}</p>
+        <img src="${symbol}" alt="">
+        <p>${number}</p>
       </div>`
   },
 
   //負責選出#cards並抽換內容
   displayCards() {
     const rootElement = document.querySelector('#cards')
-    rootElement.innerHTML = this.getCardElement()
+    rootElement.innerHTML = this.getCardElement(51)
   }
 }
 
