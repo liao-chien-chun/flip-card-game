c963a9f74d8bf5572858c20966034631c0a416df
Eric LIAO <d1044171009@gmail.com>
2022-07-06 00:10:04 +0800

    改寫flipcard and pairCard 運用展開運算子spread operator

--- main.js
+++ main.js
@@ -55,24 +55,27 @@ const view = {
   },
 
   //翻牌函式
-  flipCard (card) {
-    // console.log(card)
-    if (card.classList.contains('back')) {
-      //如果點的是背面回傳正面
-      card.classList.remove('back')
-      card.innerHTML = this.getCardContent(Number(card.dataset.index)) //使用dataset要轉成數字
-      return
-    }
-    //如果點的是正面回傳背面
-    card.classList.add('back')
-    card.innerHTML = null
+  flipCards (...cards) {
+    cards.map(card => {
+      if (card.classList.contains('back')) {
+        //如果點的是背面回傳正面
+        card.classList.remove('back')
+        card.innerHTML = this.getCardContent(Number(card.dataset.index)) //使用dataset要轉成數字
+        return
+      }
+      //如果點的是正面回傳背面
+      card.classList.add('back')
+      card.innerHTML = null
+    })
   },
 
   //配對成功函式
-  pairCard(card) {
-    card.classList.add('paired')
-  }
-
+  pairCards(...cards) {
+    cards.map(card => {
+      card.classList.add('paired')
+    })
+  },
+  
 }
 
 //model處理資料
@@ -103,27 +106,25 @@ const controller = {
     }
     switch (this.currentState) {
       case GAME_STATE.FirstCardAwaits:
-        view.flipCard(card)
+        view.flipCards(card)
         model.revealedCards.push(card)
         this.currentState = GAME_STATE.SecondCardAwaits
         break
       case GAME_STATE.SecondCardAwaits:
-        view.flipCard(card)
+        view.flipCards(card)
         model.revealedCards.push(card)
         //判斷配對是否成功
         if (model.isRevealedCardsMatched()) {
           //配對成功
           this.currentState = GAME_STATE.CardsMatched
-          view.pairCard(model.revealedCards[0])
-          view.pairCard(model.revealedCards[1])
+          view.pairCards(...model.revealedCards)
           model.revealedCards = []
           this.currentState = GAME_STATE.FirstCardAwaits
         } else {
           //配對失敗
           this.currentState = GAME_STATE.CardsMatchFailed
           setTimeout(() => {
-            view.flipCard(model.revealedCards[0])
-            view.flipCard(model.revealedCards[1])
+            view.flipCards(...model.revealedCards)
             model.revealedCards = []
             this.currentState = GAME_STATE.FirstCardAwaits
           }, 1000)
